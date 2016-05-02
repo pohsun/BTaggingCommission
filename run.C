2cea2406a85335b3d90d80f97c971a01ea91b641
Alpha <jui-fa.tsai@cern.ch>
2016-05-02 17:31:05 +0200

    Update

--- run.C
+++ run.C
@@ -1,5 +1,6 @@
 {
     gROOT->ProcessLine(".L ../TTbarSelector.C++"); 
     gROOT->ProcessLine(".L CommPlotProducer4ttbar.C++"); 
-    gROOT->ProcessLine(".x runCode4ttbarNominal.C++");
+    gROOT->ProcessLine(".x runCode4ttbar.C++");
+    //gROOT->ProcessLine(".x runCode4ttbarNominal.C++");
 }
